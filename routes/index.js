3c5ae652211428b7d5c076b4176186e419d98bda
Adi Levin <alevin@aligntech.com>
2015-01-31 21:01:51 +0200

    Use geocoding API to input addresses

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page with a given api_key in the query line.
+*  e.g. : 127.0.0.1:3000?api_key=1234
+* */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { api_key: req.query.api_key });
 });
 
 module.exports = router;
